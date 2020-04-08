295501baf33e0f1967f2fd599efb53ffbf156204
rfa-lopes <rfa.lopes@campus.fct.unl.pt>
2020-04-08 18:05:08 +0100

    update tests script

--- Server/Scripts/testReplicas.sh
+++ Server/Scripts/testReplicas.sh
@@ -3,135 +3,196 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
+TIMEFORMAT=%R
+
 tests=$1
 
+replicas=4
+
+server='https://localhost:1100'
+
+test1='/tests/test1'
+test2='/tests/test2'
+
+add='/transfers/add'
+remove='/transfers/remove'
+transf='/transfers/transfer'
+global='/transfers/globaltransfers'
+wallet='/transfers/wallettransfers/1'
+
+create='/wallets/create'
+delete='/wallets/delete/100'
+amount='/wallets/amount/1'
+info='/wallets/info/1'
+
 echo -e "\e[91mTesting:\e[39m TESTS1 - ORDERED"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/tests/test1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/tests/test1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/tests/test1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/tests/test1' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$test1;
+	time='';	
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 	
+			curl --silent --output /dev/null --insecure -X GET $var
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m TESTS2 - UNORDERED"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/tests/test2' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/tests/test2' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/tests/test2' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/tests/test2' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$test2;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X GET $var;
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m ADD MONEY"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X POST 'https://localhost:11000/transfers/add' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11001/transfers/add' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11002/transfers/add' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11003/transfers/add' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$add;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X POST $var --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}';
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m REMOVE MONEY"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X POST 'https://localhost:11000/transfers/remove' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11001/transfers/remove' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11002/transfers/remove' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11003/transfers/remove' --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$remove;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X POST $var --header 'Content-Type: application/json' --data-raw '{"id":1,"amount":5}';
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m TRANSFERS MONEY"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X POST 'https://localhost:11000/transfers/transfer' --header 'Content-Type: application/json' --data-raw '{"fromId":1,"toId":2,"amount":10}'> /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11001/transfers/transfer' --header 'Content-Type: application/json' --data-raw '{"fromId":1,"toId":2,"amount":10}'> /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11002/transfers/transfer' --header 'Content-Type: application/json' --data-raw '{"fromId":1,"toId":2,"amount":10}'> /dev/null 2>&1;
-	curl --insecure -X POST 'https://localhost:11003/transfers/transfer' --header 'Content-Type: application/json' --data-raw '{"fromId":1,"toId":2,"amount":10}'> /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$transf;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X POST $var --header 'Content-Type: application/json' --data-raw '{"fromId":1,"toId":2,"amount":10}';
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m GLOBAL TRANSFERS"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/transfers/globaltransfers' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/transfers/globaltransfers' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/transfers/globaltransfers' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/transfers/globaltransfers' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$global;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X GET $var;
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m WALLET TRANSFERS"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/transfers/wallettransfers/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/transfers/wallettransfers/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/transfers/wallettransfers/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/transfers/wallettransfers/1' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$wallet;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X GET $var;
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m CREATE WALLET"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	#curl --insecure -X POST 'https://localhost:11000/wallets/create' --header 'Content-Type: application/json' --data-raw '{"name":"TEST_USER_'$i'"}' > /dev/null 2>&1;
-	#curl --insecure -X POST 'https://localhost:11003/wallets/create' --header 'Content-Type: application/json' --data-raw '{"name":"TEST_USER_'$i'"}' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[34mWAITING TO TEST IN DOCKER\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$create;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			#curl --silent --output /dev/null --insecure -X POST $var --header 'Content-Type: application/json' --data-raw '{"name":"TEST_USER_'$i'"}';
+			#echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+			echo -ne "[$i]: \e[34mWAITING TO TEST IN DOCKER\e[39m"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m DELETE WALLET"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	#curl --insecure -X DELETE 'https://localhost:11000/wallets/delete/100' > /dev/null 2>&1;
-	#curl --insecure -X DELETE 'https://localhost:11000/wallets/delete/100' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[34mWAITING TO TEST IN DOCKER\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$delete;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			#curl --silent --output /dev/null --insecure -X DELETE $var;
+			#echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+			echo -ne "[$i]: \e[34mWAITING TO TEST IN DOCKER\e[39m"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m WALLET AMOUNT"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/wallets/amount/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/wallets/amount/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/wallets/amount/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/wallets/amount/1' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$amount;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X GET $var;
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
 
-echo;echo;
+echo;
 
 echo -e "\e[91mTesting:\e[39m WALLET INFORMATION"
-for ((i = 1 ; i <= tests ; i++ ));
-do 
-	curl --insecure -X GET 'https://localhost:11000/wallets/info/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11001/wallets/info/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11002/wallets/info/1' > /dev/null 2>&1;
-	curl --insecure -X GET 'https://localhost:11003/wallets/info/1' > /dev/null 2>&1;
-	echo -ne "[$i]: \e[92mOK\e[39m"\\r;
+for ((j = 0 ; j < replicas ; j++ ));
+do
+	var=$server$j$info;
+	for ((i = 1 ; i <= tests ; i++ ));
+	do 
+			curl --silent --output /dev/null --insecure -X GET $var;
+			echo -ne "[$i]: \e[92mOK: \e[39m $var"\\r;
+	done
+	echo;
 done
-echo;echo;
+
+echo;
+
 echo -e "\e[92mTESTS COMPLETED SUCCESSFULLY \e[39m";
 echo;
+
+#======================================================================================================================
+
+
+function avarage {
+  echo $replicas;
+}
+
+
+
 
