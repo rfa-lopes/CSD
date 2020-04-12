30781ece0a3ca81b3d383ea7bd278db988cc30cc
rfa-lopes <rfa.lopes@campus.fct.unl.pt>
2020-04-12 22:58:31 +0100

    create db script upgrade

--- Server/Scripts/create4databases.sh
+++ Server/Scripts/create4databases.sh
@@ -1,19 +1,31 @@
 if [[ $# -eq 0 ]] ; then
-    echo "./create4databases.sh <mysql_username> <mysql_password>"
+    echo "./create4databases.sh <nr db> <mysql_username[root]> <mysql_password[password]>"
     exit 1
 fi
 
-for i in {0..3}
+if [ $# -eq 1 ]
+then
+	username="root"
+	password="password"
+else
+	username=$2
+	password=$3
+fi
+
+nrdb=$1
+dropall=$((nrdb + 10))
+
+for ((i = 0 ; i < dropall ; i++ ));
 do 	
-	echo "Drop database: wallet$i";
-	mysql -u${1} -p${2} -e "DROP DATABASE wallet$i;" > /dev/null 2>&1;
+	#echo "Drop database: wallet$i";
+	mysql -u${username} -p${password} -e "DROP DATABASE wallet$i;" > /dev/null 2>&1;
 done
 
-for i in {0..3}
+for ((i = 0 ; i < nrdb ; i++ ));
 do 	
 	echo "Create database: wallet$i";	
-	mysql -u${1} -p${2} -e "CREATE DATABASE wallet$i;" > /dev/null 2>&1;
+	mysql -u${username} -p${password} -e "CREATE DATABASE wallet$i;" > /dev/null 2>&1;
 done
 
-mysql -u${1} -p${2} -e "show databases;";
-mysql -u${1} -p${2}
+mysql -u${username} -p${password} -e "show databases;";
+mysql -u${username} -p${password}
