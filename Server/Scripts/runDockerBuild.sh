d4ec628804122590056fb58d4c11031512d4c54d
Ubuntu <jl.ramalho@campus.fct.unl.pt>
2020-05-07 14:09:02 +0100

    Docker build working

--- Server/Scripts/runDockerBuild.sh
+++ Server/Scripts/runDockerBuild.sh
@@ -1,15 +1,27 @@
 docker-compose down
-
-docker network rm  appnet
+docker volume prune -f
+docker network rm appnet
 docker network create appnet
 
 docker container prune -f
-#docker image rm -f appimage
 
+#Para testar (depois de ja ter feito o download da imagem) comentar a seguinte linha
+#docker image rm -f appimage
+#Para testar (depois de ja ter feito o download da imagem) comentar a seguinte linha
 #docker build ../. -t=appimage
+
 docker-compose up -d
-sleep 20
-./startDockerApps.sh
+sleep 60
+
+echo "Starting containers............"
+docker exec -d myapp1 bash -c "./initApp1.sh"
+docker exec -d myapp2 bash -c "./initApp2.sh"
+docker exec -d myapp3 bash -c "./initApp3.sh"
+docker exec -d myapp4 bash -c "./initApp4.sh"
+
+sleep 300
+
+echo "The replicas are running!"
 
 
 
