9962b5d2b5984a2251cf57f7caa70a2bdbde79f8
rfa-lopes <rfa.lopes@campus.fct.unl.pt>
2020-06-11 10:37:49 +0100

    conflicts solve

--- Server/src/main/java/csd/wallet/Services/SmartContracts/ServiceSmartContractsClass.java
+++ Server/src/main/java/csd/wallet/Services/SmartContracts/ServiceSmartContractsClass.java
@@ -23,47 +23,30 @@ import java.util.PropertyPermission;
 @Service
 public class ServiceSmartContractsClass implements ServiceSmartContractsInterface {
 
-	@Override
-	public void executeSmartContract(SmartContract smartContract) throws Exception {
-		//if (VerifySmartContractSignature.verifies(smartContract)) {
-			//SmartContract sc = (SmartContract) smartContract.getObject();
-			byte[] sourceCodeByte = Base64.getDecoder().decode(smartContract.getCode());
-			String sourceCode = new String(sourceCodeByte);
-			String tmpProperty = System.getProperty("java.io.tmpdir");
-			Path sourcePath = Paths.get(tmpProperty, "SmartContractClient.java");
-			Files.write(sourcePath, sourceCode.getBytes("UTF-8"));
+    @Override
+    public void executeSmartContract(SmartContract smartContract) throws Exception {
+        //if (VerifySmartContractSignature.verifies(smartContract)) {
+        //SmartContract sc = (SmartContract) smartContract.getObject();
+        byte[] sourceCodeByte = Base64.getDecoder().decode(smartContract.getCode());
+        String sourceCode = new String(sourceCodeByte);
+        String tmpProperty = System.getProperty("java.io.tmpdir");
+        Path sourcePath = Paths.get(tmpProperty, "SmartContractClient.java");
+        Files.write(sourcePath, sourceCode.getBytes("UTF-8"));
 
-<<<<<<< HEAD
 		/*Permissions perms = new Permissions();
 		perms.add(new PropertyPermission("*", "read"));
 		perms.add(new RuntimePermission("getenv.TIKA_CONFIG"));*/
 
-		byte[] sourceCodeByte = Base64.getDecoder().decode(smartContract.getCode());
-		String sourceCode = new String(sourceCodeByte);
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        compiler.run(null, null, null, sourcePath.toFile().getAbsolutePath());
 
-		String tmpProperty = System.getProperty("java.io.tmpdir");
-		Path sourcePath = Paths.get(tmpProperty, "SmartContractClient.java");
-		Files.write(sourcePath, sourceCode.getBytes("UTF-8"));
-=======
-			JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-			compiler.run(null, null, null, sourcePath.toFile().getAbsolutePath());
->>>>>>> d2507565e9b9b997e28b2679b661ed11bb0d2a77
+        Path compiled = sourcePath.getParent().resolve("SmartContractClient.class");
 
-			Path compiled = sourcePath.getParent().resolve("SmartContractClient.class");
+        URL classUrl = compiled.getParent().toFile().toURI().toURL();
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{classUrl});
+        Class<?> clazz = Class.forName("SmartContractClient", true, classLoader);
+        clazz.newInstance();
 
-			URL classUrl = compiled.getParent().toFile().toURI().toURL();
-			URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{classUrl});
-			Class<?> clazz = Class.forName("SmartContractClient", true, classLoader);
-
-<<<<<<< HEAD
-		URL classUrl = compiled.getParent().toFile().toURI().toURL();
-		URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] { classUrl });
-
-		Class<?> clazz = Class.forName("SmartContractClient", true, classLoader);
-=======
-			clazz.newInstance();
->>>>>>> d2507565e9b9b997e28b2679b661ed11bb0d2a77
-
-		}
-	}
+    }
+}
 //}
